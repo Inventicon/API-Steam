0e80fcd533ea9771e27e0b980f0bdcb824c72a57
Inventicon <inventicon@users.noreply.github.com>
2018-02-21 01:55:31 -0700

    Forgot to make 'Key #' bright

--- tools/list-auth-keys.js
+++ tools/list-auth-keys.js
@@ -9,7 +9,7 @@ db.initialize().then(() => {
     core.auth.getAll().then(keys => {
         for (let i = 0; i < keys.length; i++) {
             let key = keys[i];
-            console.log(chalk.red(" = Key #" + i.toString() + " = "));
+            console.log(chalk.redBright(" = Key #" + i.toString() + " = "));
             console.log("Key", chalk.greenBright(key.key));
             console.log("Owner:", chalk.blueBright(key.owner));
             console.log("Domain:", chalk.magentaBright(key.domain));

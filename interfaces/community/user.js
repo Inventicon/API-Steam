6a5c4c52141c49df3ce4787f12a24aaf9b46ad39
Inventicon <inventicon@users.noreply.github.com>
2018-02-21 03:29:41 -0700

    Get owned games endpoint added

--- interfaces/community/user.js
+++ interfaces/community/user.js
@@ -5,6 +5,8 @@ module.exports = function GetPlayerSummaries(config, app) {
     if (!app) {
         return;
     }
+
+    // GetPlayerSummaries
     app.get(config.address + "/community/user", function (req, res) {
         let query = req.query;
 
@@ -41,6 +43,43 @@ module.exports = function GetPlayerSummaries(config, app) {
                     }).catch(error => {
                         console.log(error);
                         res.send("Server error [Steam | Community | User]");
+                    });
+                });
+            }
+        });
+    });
+
+    // GetOwnedGames
+    app.get(config.address + "/community/user/games", function (req, res) {
+        let query = req.query;
+
+        // See if required query parameters are available
+        if (query.key === undefined) {
+            res.send("No [auth] key specified!");
+            return;
+        }
+        if (query.id === undefined) {
+            res.send("No user [id] specified!");
+            return;
+        }
+
+        // Check if valid auth key
+        core.auth.exists(query.key).then(exists => {
+            if (!exists) {
+                res.send("Invalid [auth] key, not found in registry!");
+            } else {
+                core.find.user64(config.key, query.id).then(id64 => {
+                    axios.get("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/", {
+                        params: {
+                            "key": config.key,
+                            "steamid": id64
+                        }
+                    }).then(response => {
+                        console.log(response.data);
+                        res.send(response.data);
+                    }).catch(error => {
+                        console.log(error);
+                        res.send("Server error [Steam | Community | User | Games]");
                     });
                 });
             }
